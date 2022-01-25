b45ec4876cc5ac6f2bc9d4b21a9e4378f3ac7c5e
Erick Vaghi <vaghi.erick@gmail.com>
2022-01-25 18:14:34 +0100

    Hello World Code added by hand

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
