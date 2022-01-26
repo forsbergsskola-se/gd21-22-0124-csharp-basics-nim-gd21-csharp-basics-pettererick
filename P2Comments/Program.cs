539ea36a7aa48fa991ea67a3a2a819cd46248012
Erick Vaghi <vaghi.erick@gmail.com>
2022-01-26 14:30:52 +0100

    P1Comment coded

--- P2Comments/Program.cs
+++ P2Comments/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Erick Vaghi
+ * erick.vaghi@student.forsbergsskola.se
+ * Petter Soderberg
+ * petter.soderberg@student.forsbergsskola.se
+ * 26.01.22
+ */
+
+namespace Comments
+{
+    class Program
+    {
+        ///<summary>
+        /// This method prints a given line
+        /// </summary>
+        static void Main()
+        {
+            //Call a function in the Console Class that prints the string "Hello, World!"
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
