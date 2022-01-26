31f7a94a961222b4963d20e0d6387becf07ba59a
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-01-26 15:33:14 +0100

    P5 Conversion coded

--- P5Conversion/Program.cs
+++ P5Conversion/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Conversion
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = 5;
+            string numberString = number.ToString();
+            Console.WriteLine(numberString);
+            numberString = "56.3";
+            number = Convert.ToDouble(numberString);
+            Console.WriteLine(number);
+            
+        }
+    }
+}
+
