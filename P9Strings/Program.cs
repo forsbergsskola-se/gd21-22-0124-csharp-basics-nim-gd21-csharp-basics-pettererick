d8aed455b6d054a90a767a160124789acc9942c1
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-01-26 16:34:09 +0100

    P9Strings finished

--- P9Strings/Program.cs
+++ P9Strings/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("What is your first name?");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + firstname + " " + lastname + ", you are " + age + " years old.");
+        }
+    }
+}
+
